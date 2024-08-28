fe4d808913ea3e2df11aabb4b0fe2491b7831b1c
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-08-28 21:29:20 +0330

    Disable cloning forks by default
    Forks can still be cloned via `--with-forks` flag

--- clone-it-all.sh
+++ clone-it-all.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <github-username-or-organization> [output-folder]"
+usage() {
+  echo "Usage: clone-it-all <github-username-or-organization> [output-folder] [--with-forks]"
   exit 1
+}
+
+if [ -z "$1" ]; then
+  usage
 fi
 
 USER_OR_ORG=$1
 OUTPUT_DIR=${2:-$USER_OR_ORG}
+WITH_FORKS=false
+
+if [ "$3" == "--with-forks" ]; then
+  WITH_FORKS=true
+fi
 
 mkdir -p "$OUTPUT_DIR"
 
-gh repo list "$USER_OR_ORG" --limit 4000 | while read -r repo _; do
-  gh repo clone "$repo" "$OUTPUT_DIR/$(basename "$repo")"
-done
+if [ "$WITH_FORKS" = true ]; then
+  gh repo list "$USER_OR_ORG" --limit 4000 --json name --jq '.[] | .name' | while read -r repo; do
+    gh repo clone "$USER_OR_ORG/$repo" "$OUTPUT_DIR/$repo"
+  done
+else
+  gh repo list "$USER_OR_ORG" --limit 4000 --json name,isFork --jq '.[] | select(.isFork == false) | .name' | while read -r repo; do
+    gh repo clone "$USER_OR_ORG/$repo" "$OUTPUT_DIR/$repo"
+  done
+fi
