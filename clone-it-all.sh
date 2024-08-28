5f389cea9a22ae89b1f9c23858319086df6ba2d6
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-08-28 21:07:10 +0330

    Initial release 🎉

---
+++ clone-it-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <github-username-or-organization> [output-folder]"
+  exit 1
+fi
+
+USER_OR_ORG=$1
+OUTPUT_DIR=${2:-$(pwd)}
+
+mkdir -p "$OUTPUT_DIR"
+
+gh repo list "$USER_OR_ORG" --limit 4000 | while read -r repo _; do
+  gh repo clone "$repo" "$OUTPUT_DIR/$(basename "$repo")"
+done
